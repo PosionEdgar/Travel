df5ab65ac580225a6a5c8e29cae1b357bc62f557
poisonit <poisonit@163.com>
2018-09-25 16:36:42 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
-/* eslint-disable no-new */
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
